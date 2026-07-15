e8fcb85adb45f2031ba0ace0a6934a92fc937cce
陈健华 <chenjh@weipaitang.com>
2026-07-15 22:15:07 +0800

    feat: 1

--- app/hoc/page.tsx
+++ app/hoc/page.tsx
@@ -1,16 +1,38 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect, type ComponentType } from 'react';
 import { Button } from './components/button';
 import { TextArea } from './components/textarea';
 
-export default () => {
-  const [buttonVisible, setButtonVisible] = useState(false);
-  const [textareaVisible, setTextareaVisible] = useState(false);
+// 使用示例
+const withAuth = <P extends { visible: boolean }>(
+  TargetComponent: ComponentType<P>,
+  fetAuthResult: () => Promise<boolean>,
+) => {
+  return function AuthenticatedComponent(props: Omit<P, 'visible'>) {
+    const [visible, setVisible] = useState(false);
 
+    useEffect(() => {
+      fetAuthResult().then((result) => {
+        if (result) {
+          setVisible(true);
+        }
+      });
+    }, []);
+
+    return <TargetComponent {...(props as P)} visible={visible} />;
+  };
+};
+
+const fetAuthResult = () => Promise.resolve(true);
+
+const AuthButton = withAuth(Button, fetAuthResult);
+const AuthTextArea = withAuth(TextArea, fetAuthResult);
+
+export default () => {
   return (
     <div>
-      <Button visible={buttonVisible} />
-      <TextArea visible={textareaVisible} />
+      <AuthButton />
+      <AuthTextArea />
     </div>
   );
 };
